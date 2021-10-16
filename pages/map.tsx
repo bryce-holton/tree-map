24a54f73024772bf7c611b5dc34eda56efe18316
Bryce <bryce@heisenresearch.com>
2021-10-16 11:54:59 -0700

    fixed genus records

--- pages/map.tsx
+++ pages/map.tsx
@@ -1,7 +1,7 @@
 import dynamic from 'next/dynamic';
 import type { NextPage, GetStaticProps } from 'next';
 import Airtable from 'airtable';
-import { pick, map } from 'ramda';
+import { pick, map, groupBy, prop, head, path, lensPath, set } from 'ramda';
 
 const MapComponent = dynamic(
   () => import('../components/map'),
@@ -14,14 +14,21 @@ const MapPage: NextPage = (props) => <MapComponent records={[]} {...props}/>;
 export const getStaticProps: GetStaticProps = async() => {
   const baseId: string = process.env.AIRTABLE_BASE_ID!;
   const base = Airtable.base(baseId);
-  const queryRes = await base('Accession Items').select({
+  const accessionQuery = await base('Accession Items').select({
     view: 'Current Items',
     fields: ['Accession', 'Taxon Name', 'Genus', 'Species', 'Infraspecific Type 1', 'Infraspecific Name 1', 'Cultivar', 'Common Name', 'Origin', 'Family', 'Latitude', 'Longitude', 'Photo 1 URL'],
   }).all();
-  const records = map(pick(['id', 'fields']), queryRes);
+  const accessionRecords = map(pick(['id', 'fields']), accessionQuery);
+  const genusQuery = await base('Genus & Family').select({ fields: ['Genus'] }).all();
+  const genusRecords = map(pick(['id', 'fields']), genusQuery);
+  const genusGroupedById = groupBy(prop('id'), genusRecords);
+  const genusByIdClean = map(head, genusGroupedById);
+  const finalGenusRecords = map(path(['fields', 'Genus']), genusByIdClean);
+  const genusPath = lensPath(['fields', 'Genus']);
+  const mergedRecords = map((accessionRecord) => set(genusPath, finalGenusRecords[accessionRecord.fields.Genus], accessionRecord), accessionRecords);
 
   return {
-    props: { records }
+    props: { records: mergedRecords }
   }
 }
 
