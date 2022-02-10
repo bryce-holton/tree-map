2eba4b3005a2b0f0a7892538844de0a73887669b
Joe <joe@joeebner.com>
2022-02-10 14:51:50 -0800

    fixed formatting

--- components/locateButton.js
+++ components/locateButton.js
@@ -1,26 +1,26 @@
-import { useEffect } from "react"
-import { useMap } from "react-leaflet"
-import Locate from "leaflet.locatecontrol"
-import "leaflet.locatecontrol/dist/L.Control.Locate.min.css"
+import { useEffect } from 'react';
+import { useMap } from 'react-leaflet';
+import Locate from 'leaflet.locatecontrol';
+import 'leaflet.locatecontrol/dist/L.Control.Locate.min.css';
 
 const AddLocateLogic = () => {
   // Access the map context with the useMap hook
-  const map = useMap()
+  const map = useMap();
 
   // Add locate control once the map loads
   useEffect(() => {
     const locateOptions = {
-      position: "bottomright",
+      position: 'bottomright',
       // Set other options in here for locate control
       // flyTo: true,
       // drawCircle: false,
       // showPopup: false,
-    }
-    const locateControl = new Locate(locateOptions)
-    locateControl.addTo(map)
-  }, [map])
+    };
+    const locateControl = new Locate(locateOptions);
+    locateControl.addTo(map);
+  }, [map]);
 
-  return null
-}
+  return null;
+};
 
-export default AddLocateLogic
+export default AddLocateLogic;
